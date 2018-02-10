712705e2d97e142e7b7542ea87c8c36ba9ce57d8
jakes2020 <19jacris@waipahuh.k12.hi.us>
2018-02-09 14:15:04 -1000

    almost gay

---
+++ app.js
@@ -0,0 +1,30 @@
+var list = [
+	"your straight",
+	"your gay", 
+	"your super gay", 
+	"your very gay", 
+	"your very super gay",
+	"your so gay that the universe cant hold how much gay you have to you create a black hole that sucks up everything that ever existed just because of how much gay you have",
+	]
+
+
+function rng(a, b, c, d, e){
+	if (a = 0){
+		return a = var list[0]
+	};
+
+	else if (b = 1){
+		return b = var list[1]
+	}
+	else if (c = 2){
+		return c = var list[2]
+	}
+	else if (d = 3){
+		return d = var list[3]
+	}
+	else if (e = 4){
+		return e = var list[4]
+	}
+	
+	
+}
